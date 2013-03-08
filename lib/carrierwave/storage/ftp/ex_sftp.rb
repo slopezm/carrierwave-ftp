3192159deb71589671247f3696e26f5954395f06
Brandon Hansen <brandonh@ibethel.org>
2013-03-07 20:24:04 -0800

    Fix to recursive mkdir to ensure that absolute paths as well as relative paths work correctly

--- lib/carrierwave/storage/ftp/ex_sftp.rb
+++ lib/carrierwave/storage/ftp/ex_sftp.rb
@@ -2,17 +2,17 @@ require 'net/sftp'
 
 class Net::SFTP::Session
   def mkdir_p!(dir)
-    parts = dir.split("/")
-    growing_path = ""
-    for part in parts
-      next if part == ""
-      if growing_path == ""
-        growing_path = part
+    structure = Array.new
+    dir.split("/").map do |item|
+      next if item.empty?
+      if structure.empty? && !dir.start_with?("/")
+        structure << item
       else
-        growing_path = File.join(growing_path, part)
+        structure << File.join(structure.last || "", item)
       end
+
       begin
-        mkdir!(growing_path)
+        mkdir!(structure.last)
       rescue
       end
     end
